7b6cf89c1e2c581f023b5f5bb0f6f19d9635abcd
Szymon Hałucha <99204426+SzymonHalucha@users.noreply.github.com>
2024-05-16 13:14:15 +0200

    Added aggregate root test cases

--- Minerals.AutoDomain.Tests/AggregateRootGeneratorTests.cs
+++ Minerals.AutoDomain.Tests/AggregateRootGeneratorTests.cs
@@ -5,7 +5,65 @@ namespace Minerals.AutoDomain.Tests
     {
         public AggregateRootGeneratorTests()
         {
+            var references = VerifyExtensions.GetAppReferences
+            (
+                typeof(object),
+                typeof(CodeBuilder),
+                typeof(StringCases.StringExtensions),
+                typeof(DomainEventGenerator),
+                typeof(Assembly)
+            );
+            VerifyExtensions.Initialize(references);
+        }
 
+        [TestMethod]
+        public Task AggregateRoot_ShouldGenerate()
+        {
+            const string source = """
+            using Minerals.AutoDomain;
+
+            [AggregateRoot]
+            public partial class TestClass { }
+            """;
+            IIncrementalGenerator[] additional =
+            [
+                new AttributesGenerator(),
+                new InterfacesGenerator(),
+            ];
+            return this.VerifyIncrementalGenerators(source, new AggregateRootGenerator(), additional);
+        }
+
+        [TestMethod]
+        public Task AggregateRootWithNamespace_ShouldGenerate()
+        {
+            const string source = """
+            [Minerals.AutoDomain.AggregateRoot]
+            public partial class TestClass { }
+            """;
+            IIncrementalGenerator[] additional =
+            [
+                new AttributesGenerator(),
+                new InterfacesGenerator(),
+            ];
+            return this.VerifyIncrementalGenerators(source, new AggregateRootGenerator(), additional);
+        }
+
+        [TestMethod]
+        public Task AggregateRootInNamespace_ShouldGenerate()
+        {
+            const string source = """
+            namespace Minerals.Tests
+            {
+                [Minerals.AutoDomain.AggregateRoot]
+                public partial class TestClass { }
+            }
+            """;
+            IIncrementalGenerator[] additional =
+            [
+                new AttributesGenerator(),
+                new InterfacesGenerator(),
+            ];
+            return this.VerifyIncrementalGenerators(source, new AggregateRootGenerator(), additional);
         }
     }
 }
